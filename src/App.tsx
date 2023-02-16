425d4cf2f1216140439854f30df022945a330e1f
Egor-q <blizzardegor@yandex.ru>
2023-02-16 11:48:49 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from "./HelloWorld";
+import Fio from "./FIO";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <HelloWorld></HelloWorld>
+        <Fio></Fio>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/FIO.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fio = () => {
+    return (
+        <div>
+            Бушманов Максим :3 😘❤
+        </div>
+    );
+};
+
+export default Fio;
